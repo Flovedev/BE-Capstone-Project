bfde6c5a19e561918c559f3cb13387d38fe9e0a6
Flovedev <flove.dev@gmail.com>
2023-04-26 15:57:56 +0100

    updating server

---
+++ src/api/IGDB/index.ts
@@ -0,0 +1,8 @@
+import  Express  from "express";
+import createHttpError from "http-errors";
+
+const igdbRouter = Express.Router()
+
+igdbRouter.get("/genres", async (req, res, next) = > {
+   
+})
