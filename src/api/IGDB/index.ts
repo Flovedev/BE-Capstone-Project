a0c46a5a2f8241b4612822b7f26f10c94288d305
Flovedev <flove.dev@gmail.com>
2023-04-27 15:50:05 +0100

    games api working

--- src/api/IGDB/index.ts
+++ src/api/IGDB/index.ts
@@ -26,4 +26,46 @@ igdbRouter.get(
   }
 );
 
+igdbRouter.get(
+  "/platforms",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const response = await axios.post(
+        process.env.IGDB_URL + "/platforms",
+        "fields: name, platform_logo.url; limit: 200; sort name asc;",
+        {
+          headers: {
+            "Client-ID": process.env.CLIENT_ID,
+            Authorization: `Bearer ${process.env.ADMIN_TOKEN}`,
+          },
+        }
+      );
+      res.json(response.data);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+igdbRouter.get(
+  "/:where/search/:what",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const response = await axios.post(
+        process.env.IGDB_URL + "/" + req.params.where,
+        `fields: *; search: "${req.params.what}"; limit: 200;`,
+        {
+          headers: {
+            "Client-ID": process.env.CLIENT_ID,
+            Authorization: `Bearer ${process.env.ADMIN_TOKEN}`,
+          },
+        }
+      );
+      res.json(response.data);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 export default igdbRouter;
