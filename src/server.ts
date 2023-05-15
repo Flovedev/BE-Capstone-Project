ad754aa0bc9de1e3038029ad3009baae0da748cf
Flovedev <flove.dev@gmail.com>
2023-05-15 10:29:15 +0100

    update cors

--- src/server.ts
+++ src/server.ts
@@ -12,18 +12,34 @@ import passport from "passport";
 import igdbRouter from "./api/IGDB";
 import usersRouter from "./api/users";
 import gamesRouter from "./api/games";
+import createHttpError from "http-errors";
 
 // import googleStrategy from "./lib/auth/googleOauth";
 
 const expressServer = Express();
 const httpServer = createServer(expressServer);
-
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL];
 // passport.use("google", googleStrategy);
 
 //MIDDLEWARES
-expressServer.use(cors());
+expressServer.use(
+  cors({
+    origin: (currentOrigin, corsNext) => {
+      // corsNext(null, true);
+      if (!currentOrigin || whitelist.indexOf(currentOrigin) !== -1) {
+        corsNext(null, true);
+      } else {
+        corsNext(
+          createHttpError(
+            400,
+            `Origin ${currentOrigin} is not in the whitelist!`
+          )
+        );
+      }
+    },
+  })
+);
 expressServer.use(Express.json());
-
 expressServer.use(passport.initialize());
 
 //ENDPOINTS
