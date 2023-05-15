9a1cb571786254a57ee2882715b6dd66445f16f7
Flovedev <flove.dev@gmail.com>
2023-05-15 13:23:07 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-// import listEndpoints from "express-list-endpoints";
-import mongoose from "mongoose";
+const listEndpoints = require("express-list-endpoints");
+const mongoose = require("mongoose");
 import { httpServer, expressServer } from "./server";
 
 const port = process.env.PORT || 3001;
@@ -8,7 +8,7 @@ mongoose.connect(process.env.MONGO_URL as string);
 mongoose.connection.on("connected", () => {
   console.log("MongoDB connected ✅");
   httpServer.listen(port, () => {
-    // console.table(listEndpoints(expressServer));
+    console.table(listEndpoints(expressServer));
     console.log(`Server listening on port ${port}`);
   });
 });
