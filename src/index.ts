fe4b937a49a9df174271d32822b7b70fcacde5c5
Flovedev <flove.dev@gmail.com>
2023-05-15 13:25:47 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 const listEndpoints = require("express-list-endpoints");
 const mongoose = require("mongoose");
-import { httpServer, expressServer } from "./server";
+const { httpServer, expressServer } = require("./server");
 
 const port = process.env.PORT || 3001;
 mongoose.connect(process.env.MONGO_URL as string);
